45d032b9d6839f9e7622b610f9149012263451a8
marvin evins <maevins@cps.edu>
2016-12-16 22:57:04 -0600

    rolldiceApp
    roll dice app shake

--- RollDice/ViewController.swift
+++ RollDice/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rollLabel: UILabel!
+    
+    let numbers = ["0","1", "2","3","4","5","6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        let randomNumber = Int(arc4random_uniform(UInt32(numbers.count)))
+        rollLabel.text = "\(numbers[randomNumber])"
+        
+        
+        
     }
 
 
